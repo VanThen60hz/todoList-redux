8360076ac811e03c83126dec7022beacddc1f7b2
VanThen60hz <nguyenvthang2409@gmail.com>
2023-07-24 17:23:20 +0700

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,6 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-// const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
